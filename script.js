63c46948645bc99cc5c697dbb955a071b9c431a9
nPeeech <33167904+nPeeech@users.noreply.github.com>
2020-03-14 00:01:51 +0900

    git init

---
+++ script.js
@@ -0,0 +1,13 @@
+let count = 0;
+let meter_pointer = document.getElementById("meter-pointer");
+let meter_digital = document.getElementById("meter-digital");
+
+meter_pointer.style.transition = "500ms"
+meter_pointer.style.transform = "translateY(-50%) rotate(0deg)";
+meter_digital.innerText = count;
+
+meter_pointer.onclick = function(){
+	count += 45;
+	meter_pointer.style.transform = "translateY(-50%) rotate("+count+"deg)";
+	meter_digital.innerText = count*(100/180);
+};
