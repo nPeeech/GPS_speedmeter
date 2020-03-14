e64b09b80323d0b6aef149550f94c752433c3a24
nPeeech <33167904+nPeeech@users.noreply.github.com>
2020-03-14 16:57:49 +0900

    GPS test

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ meter_number_plate.onclick = function(){
 	meter_pointer.style.transform = "translateY(-50%) rotate("+count+"deg)";
 	meter_digital.innerText = count*(100/180);
 };
+
+if (navigator.geolocation) {
+  alert("この端末では位置情報が取得できます");
+// Geolocation APIに対応していない
+} else {
+  alert("この端末では位置情報が取得できません");
+}
+
+function successFunc(position) {
+	speed = position.coords.speed;
+	meter_digital.innerText = speed;
+	meter_pointer.style.transform = "translateY(-50%) rotate("+speed+"deg)";
+}
+function errorFunc(error){
+	let errorMessage = {
+		0: "原因不明のエラー",
+		1: "許可がありません",
+		2: "電波状況等",
+		3: "タイムアウト",
+	};
+	alert(errorMessage[error.code]);
+}
+let option_object = {
+	"enableHighAccuracy": false ,
+	"timeout": 1000000 ,
+	"maximumAge": 0 ,
+}
+
+navigator.geolocation.watchPosition( successFunc, errorFunc, option_object);
